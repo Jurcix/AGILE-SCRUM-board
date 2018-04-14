ee9b7ad4eb923df73a2e2eacc70e65d209ec078d
Jurate Pakalnyte <pakalnyte.jurate@gmail.com>
2018-04-14 22:03:46 +0300

    feat/added logic, contected to store (#8)

--- src/app/store/project/effects.ts
+++ src/app/store/project/effects.ts
@@ -1,4 +1,5 @@
-import { FilteredUsers, ProjectSummary } from './../../types/project';
+import { ActivatedRoute } from '@angular/router';
+import { FilteredUsers, ProjectSummary, ProjectDetails } from './../../types/project';
 import { Observable } from 'rxjs/Observable';
 import { AppState } from './../index';
 import { Store } from '@ngrx/store';
@@ -7,9 +8,11 @@ import { Injectable } from '@angular/core';
 import { Effect, Actions } from '@ngrx/effects';
 
 import * as ProjectActions from './actions';
-import { map, switchMap, catchError } from 'rxjs/operators';
+import { map, switchMap, catchError, take, withLatestFrom } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 import { of } from 'rxjs/observable/of';
+import { ProfileActions } from '../profile';
+import { getRouterState } from '../app-routes';
 
 @Injectable()
 export class ProjectEffects {
@@ -18,6 +21,7 @@ export class ProjectEffects {
     private http: HttpClient,
     private actions$: Actions,
     private store: Store<AppState>,
+    private route: ActivatedRoute,
   ) { }
 
   @Effect()
@@ -26,13 +30,19 @@ export class ProjectEffects {
     .pipe(
       map((action: ProjectActions.ProjectCreateRequest) => action.payload),
       switchMap((payload) => {
-        console.log(payload)
         return this.http.post(`${environment.apiURL}v1/projects/create-project`, payload)
           .pipe(
             map(() => new ProjectActions.ProjectCreateSuccess()),
             catchError(err => of(new ProjectActions.ProjectCreateError(err))),
           );
       })
+    );
+
+  @Effect()
+  $createProjectSucces: Observable<ProjectActions.ProjectActions> = this.actions$
+    .ofType(ProjectActions.PROJECT.CREATE.SUCCESS)
+    .pipe(
+      map(() => new ProjectActions.ProjectSummaryRequest())
     );
 
   @Effect()
@@ -58,6 +68,60 @@ export class ProjectEffects {
           .pipe(
             map((data: ProjectSummary[]) => new ProjectActions.ProjectSummarySuccess(data)),
             catchError(err => of(new ProjectActions.ProjectSummaryError(err))),
+          );
+      })
+    );
+
+  @Effect()
+  $ProjectDetailsRequest: Observable<ProjectActions.ProjectActions> = this.actions$
+    .ofType(ProjectActions.PROJECT.DETAILS.REQUEST)
+    .pipe(
+      withLatestFrom(this.store.select(getRouterState)),
+      switchMap(([action, router]) => {
+        const id = router.state.params.id;
+        return this.http.get(`${environment.apiURL}v1/projects/${id}/project-details`)
+          .pipe(
+            map((data: ProjectDetails) => new ProjectActions.ProjectDetailsSuccess(data)),
+            catchError(err => of(new ProjectActions.ProjectSummaryError(err))),
+          );
+      })
+    );
+
+  @Effect()
+  $RemoveUserRequest: Observable<ProjectActions.ProjectActions> = this.actions$
+    .ofType(ProjectActions.REMOVE_USER.REQUEST)
+    .pipe(
+      map((action: ProjectActions.RemoveUserRequest) => action.payload),
+      withLatestFrom(this.store.select(getRouterState)),
+      switchMap(([payload, router]) => {
+        const id = router.state.params.id;
+        return this.http.patch(`${environment.apiURL}v1/projects/${id}/remove-user`, payload)
+          .pipe(
+            map((data: ProjectDetails) => new ProjectActions.RemoveUserSuccess(data)),
+            catchError(err => of(new ProjectActions.RemoveUserError(err))),
+          );
+      })
+    );
+
+  @Effect()
+  $RemoveUserSucces: Observable<ProjectActions.ProjectActions> = this.actions$
+    .ofType(ProjectActions.REMOVE_USER.SUCCESS)
+    .pipe(
+      map(() => new ProjectActions.ProjectDetailsRequest())
+    );
+
+  @Effect()
+  $AddUsers: Observable<ProjectActions.ProjectActions> = this.actions$
+    .ofType(ProjectActions.ADD_USERS.REQUEST)
+    .pipe(
+      map((action: ProjectActions.AddUsersRequest) => action.payload),
+      withLatestFrom(this.store.select(getRouterState)),
+      switchMap(([payload, router]) => {
+        const id = router.state.params.id;
+        return this.http.patch(`${environment.apiURL}v1/projects/${id}/update-project`, payload)
+          .pipe(
+            map((data: ProjectDetails) => new ProjectActions.AddUsersSuccess(data)),
+            catchError(err => of(new ProjectActions.AddUsersError(err))),
           );
       })
     );
